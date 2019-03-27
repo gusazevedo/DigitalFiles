446e1fe7718a311d97c816e9ede49b1e8a255d1a
gusazevedo <gustavonovaisgan@gmail.com>
2019-03-27 14:56:08 -0300

    Estilização do menu lateral

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
-/* 
- *  ABAS DE NAVEGACAO
-*/
-
 var active_aba = '';
 var prev_aba = '';
 var next_aba = '';
 
 $('a[data-toggle="pill"]').on('show.bs.tab', function (e) {
     active_aba = e.target;
-    // prev_aba = e.relatedTarget;
-    prev_aba = $(active_aba).prev();
-    next_aba = $(active_aba).next();
+// prev_aba = e.relatedTarget;
+prev_aba = $(active_aba).prev();
+next_aba = $(active_aba).next();
 });
 
 $('#next_aba').click(function(){
