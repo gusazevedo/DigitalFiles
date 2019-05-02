ca859ffcadd4635420caace7b8f477e55225065a
gusazevedo <gustavonovaisgan@gmail.com>
2019-05-02 14:17:16 -0300

    Adição de um controlador de auto-contraste e de fonte para melhor leitura

--- js/script.js
+++ js/script.js
@@ -1,30 +1,56 @@
-/*
-*  ABAS DE NAVEGACAO
-*/
+/* ABAS DE NAVEGACAO */
 
 var active_aba = '';
 var prev_aba = '';
 var next_aba = '';
 
 $('a[data-toggle="pill"]').on('show.bs.tab', function (e) {
-   active_aba = e.target;
-   // prev_aba = e.relatedTarget;
-   prev_aba = $(active_aba).prev();
-   next_aba = $(active_aba).next();
+    active_aba = e.target;
+    // prev_aba = e.relatedTarget;
+    prev_aba = $(active_aba).prev();
+    next_aba = $(active_aba).next();
 
-	window.scroll({
-		top: $('#topo').height() + 10,
-		left: 0,
-		behavior: 'smooth'
-	});    
+    window.scroll({
+        top: $('#topo').height() + 480,
+        left: 0,
+        behavior: 'smooth'
+    });
 });
 
 $('#next_aba').click(function(){
-   $(next_aba).tab('show');
+    $(next_aba).tab('show');
 });
 
 $('#prev_aba').click(function(){
-   $(prev_aba).tab('show');
+    $(prev_aba).tab('show');
 });
 
 $('#aba_1').tab('show');
+
+/*  TOOLTIPS  */
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('#element').tooltip('toggleEnabled');
+})
+
+/*  CONTROLE DE FONTES  */
+
+$('.fonte-aumentar').click(function(){
+    $('.aba p').css('font-size','+=3');
+});
+
+$('.fonte-normal').click(function(){
+    $('.aba p').css('font-size','1rem');
+});
+
+$('.fonte-diminuir').click(function(){
+    $('.aba p').css('font-size','-=3');
+});
+
+/* CONTRASTE */
+
+$('.contraste').click(function(){
+    $("body").toggleClass('sup-contraste');
+    // $("body .topo .separador").toggleClass('separador');
+});
